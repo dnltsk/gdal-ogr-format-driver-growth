bbe6bbb829362386d9d6fbc1fa73d32d093454a5
Daniel Teske <daniel.teske@meteogroup.com>
2016-02-03 21:48:58 +0100

    added 2.1.0; removed yaxis; added MongoDb+WMTS; moved legend ul

--- growth.r
+++ growth.r
@@ -1,9 +1,3 @@
-install.packages("xkcd")
-install.packages("htmlTable")
-install.packages("reshape2")
-install.packages("extrafont")
-install.packages("ggplot2")
-install.packages("cowplot")
 library("XML")       #readHTMLTable
 library("extrafont")
 library("zoo")       
@@ -13,11 +7,13 @@ library("ggplot2")
 library("xkcd")
 library("cowplot")   #ggdraw
 
-setwd("/projects_small/gdal-versions/")
+LC_ALL="en_EN.utf8 date"
+
+setwd("/projects_small/gdal-ogr-format-driver-growth/")
 
 # XKCD Font
 download.file("http://simonsoftware.se/other/xkcd.ttf",
-                             dest="xkcd.ttf", mode="wb")
+              dest="xkcd.ttf", mode="wb")
 font_import(".", prompt=FALSE)
 if(.Platform$OS.type != "unix") {
   ## Register fonts for Windows bitmap output
@@ -48,6 +44,7 @@ xkcd_axis <- function() {
 
 data <- read.table(header = TRUE, text = "
 version date
+2.1.0 05-2016
 2.0.0 06-2015
 1.11.0 04-2014
 1.10.0 04-2013
@@ -82,25 +79,39 @@ data$ogr <- apply(data, 1, function(x){countOgrFormats(x[[1]])})
 
 melted <- melt(data, id.vars=c("version", "date"))
 
+x_date_labels <- function(currentDate) {
+  trunkDate <- data[which(data$version == "2.1.0"),]$date
+  currentData <- data[which(data$date == currentDate),]
+  ifelse(
+    trunkDate == currentDate,
+    paste("future:",currentData$version, "-", format(currentData$date, "%b %Y")),
+    paste(currentData$version, "-", format(currentData$date, "%b %Y"))
+  )
+}
+
 p <- ggplot(melted, aes(date, value, color = variable)) +
   ggtitle("GDAL-OGR: continues growth of format drivers") +
   geom_line(aes(group=variable)) +
   theme_xkcd() +
   #axes
   theme(axis.text.x = element_text(angle = 45, hjust=1, vjust=1)) +
-  scale_x_date(name="version and date",
+  scale_x_date(name=NULL,
                breaks=data$date, 
-               labels=paste(data$version, "-", format(data$date, "%b %Y"))) +
-  scale_y_continuous(name="num of formats",
-                     breaks=seq(0,150,25)) +
+               #labels=paste(data$version, "-", format(data$date, "%b %Y")),
+               labels=x_date_labels,
+               limits=c(min(data$date)-150, max(data$date)+200)) +
+  #scale_y_continuous(name="num of formats", breaks=seq(0,150,25)) +
+  scale_y_continuous(name=NULL,breaks=NULL) +
   #label
-  geom_label(aes(fill= variable, label=value), colour = "white", fontface = "bold", family = "xkcd", label.size = 1, show.legend=F) +
+  geom_label(aes(fill= variable, label=value), colour = "white", fontface = "bold", family = "xkcd", size=5, label.size = 1, show.legend=F) +
   #legend
-  theme(legend.position="bottom", 
+  theme(legend.justification=c(0,1), 
+        legend.position=c(0,1), 
         legend.title=element_blank(),
         plot.title = element_text(size=30, face="bold")) +
+  
   scale_colour_discrete(breaks=c("gdal", "ogr"),
-                        labels=c("num of raster formats", "num of vector formats"))
+                        labels=c("\nnumber of\nraster formats\n", "number of\nvector formats"))
 
 
 highs <- read.table(header = TRUE, text = "
@@ -109,16 +120,26 @@ WMS 1.5.0 12-2007 gdal 90
 BigTIFF 1.5.0 05-2007 gdal 110
 Grib 1.6.0 12-2008 gdal 110
 R 1.7.1 02-2010 gdal 120
-Rasterlite 1.7.1 10-2010 gdal 90
+Rasterlite 1.7.1 08-2010 gdal 90
+WMTS 2.1.0 05-2016 gdal 130
 SpatiaLite 1.7.1 07-2010 ogr 25
 CartoDB 1.11.0 04-2014 ogr 91
 WFS 1.8.0 01-2011 ogr 66
-ES 1.10.0 04-2013 ogr 55
-GeoJSON 1.5.0 12-2007 ogr 44")
+ElasticSearch 1.10.0 10-2013 ogr 55
+GeoJSON 1.5.0 12-2007 ogr 44
+MongoDB 2.1.0 05-2016 ogr 65
+")
 
 highs$date <- as.Date(as.yearmon(highs$date, "%m-%Y"))
-p <- p + geom_label(data=highs, aes(date, hight, group=variable, label=driver), fill="yellow", fontface = "bold", size=9, family = "xkcd", label.size = 0, show.legend = F)
+p <- p + geom_label(data=highs, aes(date, hight, group=variable, label=driver), fill="yellow", fontface = "bold", size=7, family = "xkcd", label.size = 0, show.legend = F)
+
+#p <- p + geom_label(data=highs, aes(date, hight, group=variable, label=driver), fill="yellow", fontface = "bold", size=7, family = "xkcd", label.size = 0, show.legend = F)
+
+png("time-series.png", width=700, height=400)
+p
+dev.off()
 
 
-ggdraw(switch_axis_position(p, axis = 'y'))
+#ggdraw(switch_axis_position(p, axis = 'y'))
 
+#ggsave("plot.png", width=14, height=8, dpi=96, units="in")
